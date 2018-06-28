4ad01c6ccfd71af50d8eeac1d9a79d818d5884b1
Tim DenOuden <timothy.denouden@gmail.com>
2018-06-28 09:55:55 -0700

    added login activity

---
+++ App/app/src/main/java/com/avanade/artmachina/app/activities/LogInActivity.java
@@ -0,0 +1,15 @@
+package com.avanade.artmachina.app.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.avanade.artmachina.R;
+
+public class LogInActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_log_in);
+    }
+}
